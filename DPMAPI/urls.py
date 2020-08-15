fea54fa41db66b08e12a42f6e60d66b2ab03df54
rajesh-danphe <54258771+rajesh-danphe@users.noreply.github.com>
2020-08-15 14:09:59 +0530

    path changes

--- DPMAPI/urls.py
+++ DPMAPI/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from DPMAPI import views
+from DPM.DPMAPI import views
 
 urlpatterns = [
     url('', views.centroids),
--- DPMMain/urls.py
+++ DPMMain/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
-from DPMAPI import views
+from DPM.DPMAPI import views
 
 # router = routers.DefaultRouter()
 # router.register(r'Centroid', views.centroids)
