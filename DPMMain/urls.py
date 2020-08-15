7f2510ebec8eac0a2895858532389a1de9257682
rajesh-danphe <54258771+rajesh-danphe@users.noreply.github.com>
2020-08-15 14:27:53 +0530

    changes

--- DPMMain/urls.py
+++ DPMMain/urls.py
@@ -2,16 +2,7 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
-from rest_framework import routers
-from DPMAPI import views
-
-# router = routers.DefaultRouter()
-# router.register(r'Centroid', views.centroids)
-# router.register(r'Predicted', views.predicted)
-
 urlpatterns = [
     url('admin/', admin.site.urls),
     path('Centroids',include('DPMAPI.urls')),
