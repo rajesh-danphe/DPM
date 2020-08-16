3b2a6d1346e8a9df69b271ee578a8fd78f2e7ee3
rajesh-danphe <54258771+rajesh-danphe@users.noreply.github.com>
2020-08-16 17:07:20 +0530

    wsgi changes

--- DPMMain/asgi.py
+++ DPMMain/asgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 # & e:/ShivSirProject/Django/venv/Scripts/Activate.ps1 to activate venv
 import os
-from whitenoise import WhiteNoise
 from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DPMMain.settings')
-
 application = get_asgi_application()
 
-application = WhiteNoise(application, root='DPMAPI')
-application.add_files('DPMAPI', prefix='static/')
