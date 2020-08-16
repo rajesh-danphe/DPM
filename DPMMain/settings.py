89a9931d05c889504a13bf6f687231a12c35c159
rajesh-danphe <54258771+rajesh-danphe@users.noreply.github.com>
2020-08-16 17:15:39 +0530

    Update settings.py

--- DPMMain/settings.py
+++ DPMMain/settings.py
@@ -140,4 +140,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'DPMAPI\static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
